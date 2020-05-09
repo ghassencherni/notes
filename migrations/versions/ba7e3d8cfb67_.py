bdaf632eb80ce1456ec2382117e82f4e4379a466
ghassen charni <ghassen.cherni@gmail.com>
2020-05-10 00:34:38 +0200

    delete migrations dir

--- migrations/versions/ba7e3d8cfb67_.py
+++ migrations/versions/ba7e3d8cfb67_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 91868aac57a8
+Revision ID: ba7e3d8cfb67
 Revises: 
-Create Date: 2020-05-09 02:47:52.837985
+Create Date: 2020-05-09 04:36:10.356004
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '91868aac57a8'
+revision = 'ba7e3d8cfb67'
 down_revision = None
 branch_labels = None
 depends_on = None
